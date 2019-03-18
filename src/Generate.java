999046d7665d86706e25513b0682de5c0c5cd10c
hartley9 <hartley9@lancaster.ac.uk>
2019-03-18 20:17:31 +0000

    mostly done, need to indent

--- src/Generate.java
+++ src/Generate.java
@@ -1,6 +1,10 @@
 public class Generate extends AbstractGenerate {
 
-    public void reportError(Token token, String explanatoryMessage) throws CompilationException{}
+    public void reportError(Token token, String explanatoryMessage) throws CompilationException
+    {
+        System.out.println(explanatoryMessage + " but found " + token.text);
+        throw new CompilationException(explanatoryMessage, token.lineNumber);
+    }
 
 
 }
--- src/SyntaxAnalyser.java
+++ src/SyntaxAnalyser.java
@@ -22,13 +22,27 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
                 myGenerate.reportError(nextToken, "Begin Expected");
             }
         _statementList_();
-
-        //if (nextToken.symbol==8)
-        //{}
+        if (nextToken.symbol==8)
+        {
+            myGenerate.insertTerminal(nextToken);
+        } else
+            {
+                myGenerate.reportError(nextToken, "Expected end");
+            }
+        nextToken=lex.getNextToken();
+        if(nextToken.symbol==10)
+        {
+            myGenerate.insertTerminal(nextToken);
+        }
+        else
+            {
+                myGenerate.reportError(nextToken, "expected end of file");
+            }
     }
     //Sort out logic here
     public void _statementList_() throws IOException, CompilationException
     {
+        myGenerate.commenceNonterminal("statementList");
         nextToken = lex.getNextToken();
 
         if(nextToken.symbol==16 || nextToken.symbol==17 || nextToken.symbol==36 || nextToken.symbol==3 || nextToken.symbol==7||nextToken.symbol==37)
@@ -39,18 +53,25 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
         {
 
         }
-        else if(nextToken.symbol == 30)
+        else
+        {
+
+        }
+        //System.out.println(nextToken.text);
+        if (nextToken.symbol==30)
+        {
+            myGenerate.insertTerminal(nextToken);
+        }
+        if((nextToken.symbol != 8) && (nextToken.symbol!=10))
         {
             _statementList_();
         }
-        else
-            {
 
-            }
     }
 
     public void _statement_() throws IOException, CompilationException
     {
+        myGenerate.commenceNonterminal("statement");
         if(nextToken.symbol==16)
         {
             _assignStatement_();
@@ -75,22 +96,31 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
         {
             _forStatement_();
         }
+        //System.out.println(nextToken.text);
+        //nextToken=lex.getNextToken();
+        //System.out.println(nextToken.text);
+        myGenerate.finishNonterminal("statement");
     }
 
     public void _assignStatement_() throws IOException, CompilationException
     {
+        myGenerate.commenceNonterminal("assignStatements");
         if (nextToken.symbol == 16)
         {
             myGenerate.insertTerminal(nextToken);
             nextToken = lex.getNextToken();
             //Check for = symbol
-            if (nextToken.symbol == 11)
+            System.out.println(nextToken.text);
+            if (nextToken.symbol == 1)
             {
+                myGenerate.insertTerminal(nextToken);
                 nextToken = lex.getNextToken();
+                //System.out.println(nextToken.text);
                 //Check for string constant
                 if (nextToken.symbol == 31)
                 {
                     myGenerate.insertTerminal(nextToken);
+
                 }
                 //If not string constant then is expression
                 else
@@ -103,11 +133,13 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
                     myGenerate.reportError(nextToken, "Expected =");
                 }
         }
+        myGenerate.finishNonterminal("assignStatement");
 
     }
 
     public void _ifStatement_() throws IOException, CompilationException
     {
+        myGenerate.commenceNonterminal("ifStatement");
         if (nextToken.symbol==17)
         {
             myGenerate.insertTerminal(nextToken);
@@ -145,10 +177,12 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
             {
                 myGenerate.reportError(nextToken, "Expected end");
             }
+        myGenerate.finishNonterminal("ifStatement");
     }
 
     public void _whileStatement_() throws IOException, CompilationException
     {
+        myGenerate.commenceNonterminal("whileStatement");
         //Expecting while
         if (nextToken.symbol==36)
         {
@@ -158,7 +192,7 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
             {
                 myGenerate.reportError(nextToken, "Expected while");
             }
-        nextToken=lex.getNextToken();
+    //    nextToken=lex.getNextToken();
         //Expecting condition
         _condition_();
         nextToken=lex.getNextToken();
@@ -171,10 +205,10 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
             {
                 myGenerate.reportError(nextToken, "Expected loop.");
             }
-        nextToken=lex.getNextToken();
+        //nextToken=lex.getNextToken();
         //Expecting statement
         _statementList_();
-        nextToken=lex.getNextToken();
+     //   nextToken=lex.getNextToken();
         //Expecting end symbol
         if (nextToken.symbol==8)
         {
@@ -194,10 +228,12 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
             {
                 myGenerate.reportError(nextToken, "Expected loop");
             }
+        myGenerate.finishNonterminal("whileStatement");
     }
 
     public void _procedureStatement_() throws IOException, CompilationException
     {
+        myGenerate.commenceNonterminal("procedureStatement");
         //Expecting call
         if (nextToken.symbol==3)
         {
@@ -209,7 +245,7 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
             }
         nextToken=lex.getNextToken();
         //Expecting identifier
-        if (nextToken==16)
+        if (nextToken.symbol==16)
         {
             myGenerate.insertTerminal(nextToken);
         }
@@ -219,6 +255,7 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
             }
         nextToken=lex.getNextToken();
         //Expecting left parenthesis
+        //System.out.println(nextToken.text + "\n\n");
         if(nextToken.symbol==20)
         {
             myGenerate.insertTerminal(nextToken);
@@ -230,7 +267,7 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
         nextToken=lex.getNextToken();
         //Expecting argument list
         _argumentList_();
-        nextToken=lex.getNextToken();
+        //nextToken=lex.getNextToken();
         //Expecting right parenthesis
         if(nextToken.symbol==29)
         {
@@ -240,10 +277,13 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
             {
                 myGenerate.reportError(nextToken, "Expected right parenthesis");
             }
+        nextToken=lex.getNextToken();
+        myGenerate.finishNonterminal("procedureStatement");
     }
 
     public void _untilStatement_() throws IOException, CompilationException
     {
+        myGenerate.commenceNonterminal("untilStatement");
         //Expecting do
         if (nextToken.symbol == 7)
         {
@@ -270,10 +310,12 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
         nextToken=lex.getNextToken();
         //Expecting condition
         _condition_();
+        myGenerate.finishNonterminal("untilStatement");
     }
 
     public void _forStatement_() throws IOException, CompilationException
     {
+        myGenerate.commenceNonterminal("forStatement");
         //Expecting for
         if (nextToken.symbol == 37)
         {
@@ -283,9 +325,223 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
             {
                 myGenerate.reportError(nextToken, "Expected for");
             }
+        //Expecting lft parenthesis
+        nextToken=lex.getNextToken();
+        if(nextToken.symbol==20)
+        {
+            myGenerate.insertTerminal(nextToken);
+        }
+        else
+            {
+                myGenerate.reportError(nextToken, "Expected left parenthesis");
+            }
+        //Expecting assignment statement
+        nextToken=lex.getNextToken();
+        _assignStatement_();
+        //Expecting semi colon
+        nextToken=lex.getNextToken();
+        if (nextToken.symbol==30)
+        {
+            myGenerate.insertTerminal(nextToken);
+        }
+        else
+            {
+                myGenerate.reportError(nextToken, "Expected semi colon");
+            }
 
-        
+        //Expecting condition
+        nextToken=lex.getNextToken();
+        _condition_();
+        //Expecting semi colon
+        nextToken=lex.getNextToken();
+        if (nextToken.symbol==30)
+        {
+            myGenerate.insertTerminal(nextToken);
+        }
+        else
+        {
+            myGenerate.reportError(nextToken, "Expected semi colon");
+        }
+        //Expecting assignment statement
+        nextToken=lex.getNextToken();
+        _assignStatement_();
 
+        //Expecting right parenthesis
+        nextToken=lex.getNextToken();
+        if(nextToken.symbol == 29)
+        {
+            myGenerate.insertTerminal(nextToken);
+        }
+        else
+            {
+                myGenerate.reportError(nextToken, "Expected right parenthesis");
+            }
+        //Expecting do
+        nextToken=lex.getNextToken();
+        if(nextToken.symbol == 7)
+        {
+            myGenerate.insertTerminal(nextToken);
+        }
+        else
+            {
+                myGenerate.reportError(nextToken,"Expected do");
+            }
+        //Expecting statement list
+        nextToken=lex.getNextToken();
+        _statementList_();
+        //Expecing end
+        nextToken=lex.getNextToken();
+        if(nextToken.symbol == 8)
+        {
+            myGenerate.insertTerminal(nextToken);
+        }
+        else
+            {
+                myGenerate.reportError(nextToken, "Expected end");
+            }
+        //Expecting loop
+        nextToken=lex.getNextToken();
+        if(nextToken.symbol == 23)
+        {
+            myGenerate.insertTerminal(nextToken);
+        }
+        else
+            {
+                myGenerate.reportError(nextToken,"Expected loop");
+            }
+        myGenerate.finishNonterminal("forStatement");
+    }
+
+    public void _argumentList_() throws IOException, CompilationException
+    {
+        myGenerate.commenceNonterminal("argumentList");
+        //nextToken=lex.getNextToken();
+        //Expecting identifer or agument list
+        if (nextToken.symbol==16)
+        {
+            myGenerate.insertTerminal(nextToken);
+        }
+        else
+            {
+                myGenerate.reportError(nextToken, "Expected Identifier");
+            }
+        nextToken=lex.getNextToken();
+     //   System.out.println(nextToken.text);
+        if (nextToken.symbol==5)
+        {
+            myGenerate.insertTerminal(nextToken);
+            _argumentList_();
+        }
+        myGenerate.finishNonterminal("argumentList");
+    }
+
+    public void _condition_() throws IOException, CompilationException
+    {
+        myGenerate.commenceNonterminal("condition");
+        nextToken=lex.getNextToken();
+        //System.out.println(nextToken.text);
+        if(nextToken.symbol==16)
+        {
+            myGenerate.insertTerminal(nextToken);
+        }
+        else
+            {
+                myGenerate.reportError(nextToken,"Expected identifier");
+            }
+
+        //Expected conditional operator
+       // nextToken=lex.getNextToken();
+        _conditionalOperator_();
+        //Expect identifier, number constant or string constant
+        nextToken=lex.getNextToken();
+        if ((nextToken.symbol == 16) | (nextToken.symbol==26) | (nextToken.symbol==31))
+        {
+            myGenerate.insertTerminal(nextToken);
+        }
+        else
+            {
+                myGenerate.reportError(nextToken, "Expected identifier/numberConstant/stringConstant");
+            }
+        myGenerate.finishNonterminal("conditional");
+    }
+
+    public void _conditionalOperator_() throws IOException, CompilationException
+    {
+        myGenerate.commenceNonterminal("conditionalOperator");
+        nextToken=lex.getNextToken();
+        if ((nextToken.symbol==15) | (nextToken.symbol==14) | (nextToken.symbol==11) | (nextToken.symbol==25) | (nextToken.symbol==22) | (nextToken.symbol==21))
+        {
+            myGenerate.insertTerminal(nextToken);
+        }
+        else
+            {
+                myGenerate.reportError(nextToken, "Expected operator symbol");
+            }
+        myGenerate.finishNonterminal("conditionalOperator");
+    }
+
+    public void _expression_() throws IOException, CompilationException
+    {
+        myGenerate.commenceNonterminal("expression");
+        //nextToken=lex.getNextToken();
+        _term_();
+
+        if ((nextToken.symbol==27) | (nextToken.symbol==24))
+        {
+            myGenerate.insertTerminal(nextToken);
+            nextToken=lex.getNextToken();
+            _expression_();
+        }
+        myGenerate.finishNonterminal("expression");
+
+    }
+
+    public void _term_() throws IOException, CompilationException
+    {
+        myGenerate.commenceNonterminal("term");
+        _factor_();
+        nextToken=lex.getNextToken();
+        //Expecting plus or minus
+        if ((nextToken.symbol==27) | (nextToken.symbol==24))
+        {
+
+        }else if((nextToken.symbol==33)|(nextToken.symbol==6))
+        {
+            nextToken=lex.getNextToken();
+            _term_();
+        }
+        myGenerate.finishNonterminal("term");
+    }
+
+    public void _factor_() throws IOException, CompilationException
+    {
+        myGenerate.commenceNonterminal("factor");
+        //System.out.println(nextToken.text);
+        if ((nextToken.symbol==16)||(nextToken.symbol==26) || (nextToken.symbol==13) ||(nextToken.symbol==18))
+        {
+            myGenerate.insertTerminal(nextToken);
+
+        }else
+            {
+                if (nextToken.symbol==20)
+                {
+                    myGenerate.insertTerminal(nextToken);
+                    _expression_();
+                    if (nextToken.symbol==29)
+                    {
+                        myGenerate.insertTerminal(nextToken);
+                    }
+                    else
+                        {
+                            myGenerate.reportError(nextToken, "Expected right parenthesis");
+                        }
+                }else
+                    {
+                        System.out.println(nextToken.text);
+                        myGenerate.reportError(nextToken, "Expected identifier/numberconstant/bracket");
+                    }
+            }
+        myGenerate.finishNonterminal("factor");
     }
 
     //
